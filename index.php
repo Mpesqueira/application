c30adcb1f1d4bab8541bd8ceaf4738449ab8a0fa
Mpesqueira <pesqueiramark@gmail.com>
2023-04-25 20:42:13 -0400

    Create home page and add image

--- index.php
+++ index.php
@@ -18,11 +18,11 @@ $f3 = Base::instance();
 
 // Define a default route
 $f3->route('GET /', function() {
-    echo '<h1>Hello World!</h1>';
+    //echo '<h1>Hello World!</h1>';
 
     // Display a view page
-    //$view = new Template();
-    //echo $view->render('views/home.html');
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
 // Run Fat-Free
